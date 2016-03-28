66516a0a190c1c8f583d6ec23fb6cba69cea2881
Arthur.yu <arthur.yu@centling.com>
2016-03-28 11:07:46 +0800

    创建

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by Arthur.yu on 16/3/28.
+//  Copyright © 2016年 Arthur.yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
